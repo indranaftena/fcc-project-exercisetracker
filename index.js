c5631fb11b0e5bf809d4257b25dd9b3425d739b0
Indra Kurniawan <114134339+indranaftena@users.noreply.github.com>
2022-11-24 02:03:22 +0000

    change from body-parser to express.json

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const myURI = process.env['MONGO_URI']
 
 app.use(cors())
 app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // connect to MongoDB
 mongoose.connect(myURI, {
